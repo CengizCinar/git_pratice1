ed9bfdb577bb187dc24c7894ff1010f535b5aa47
CengizCinar <bandirmausak@gmail.com>
2022-03-30 14:48:39 +0200

    Work 3 is done  and about to be pushed to Github

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     //type anything
 
 // Doing awesome work 2 !
+// Doing awesome work 3  is done ad ready to be committed!
+
+    //and ready to be pushed to remote repository
+
 
 
 
